37fc4152a6e9674baf34395ed53ae4b5d5027cca
Eric Heredia <ericdavidheredia@gmail.com>
2019-08-04 14:35:44 -0500

    inital retweet working

--- bot.js
+++ bot.js
@@ -1,5 +1,32 @@
-require('dotenv').config({path: __dirname + '/.env'});
 const twit = require('twit');
-
+const config = require('./config')
 const Twitter = new twit(config);
 
+const retweet = () => {
+  const params = {
+    q: '#ReactJS, #javascript, #MongoDB, #NodeJS, #coding',
+    result_type: 'recent',
+    lang: 'en',
+    count: '15'
+  }
+
+Twitter.get('search/tweets', params, (err, data, res) => {
+  if (err) {
+    console.log(err.message);
+  }
+  console.log(data.statuses[0].id_str);
+  Twitter.post('statuses/retweet/' + data.statuses[0].id_str, (err, data, res) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log('Retweeted!');
+    }
+  })
+})
+}
+
+retweet();
+console.log('\033[2J');
+console.log('Running retweet...');
+
+setInterval(retweet, 10000);
--- config.js
+++ config.js
@@ -4,9 +4,11 @@
  * consumer_secret
  * access_token
  * access_token_secret
- */module.exports = {
-  consumer_key: '',  
-  consumer_secret: '',
-  access_token: '',  
-  access_token_secret: ''
+ */
+require('dotenv').config({path: __dirname + '/.env'});
+module.exports = {
+  consumer_key: process.env.CONSUMER_KEY,  
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN_KEY,  
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 }
